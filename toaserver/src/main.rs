9fa2df16a755e4b8dba1130a029bc088f0058f12
memoryleak47 <memoryleak47@protonmail.ch>
2018-09-23 16:05:54 +0200

    toaserver: unified PlayerPool and NetPool into UserPool

--- toaserver/src/main.rs
+++ toaserver/src/main.rs
@@ -1,30 +1,28 @@
 extern crate toalib;
 
 use toalib::world::World;
-use toalib::team::{Team, PlayerPool};
+use toalib::team::Team;
 use toalib::packet::{ServerToClientPacket, ClientToServerPacket};
 
 mod pool;
 mod net;
 mod term;
 
-use self::pool::NetPool;
+use self::pool::UserPool;
 use self::net::create_listener;
 use self::term::{Term, TermCommand};
 
 fn main() {
 	let listener = create_listener().expect("Could not create listener");
 
-	let mut net_pool = NetPool::new();
-	let mut player_pool = PlayerPool::new();
+	let mut user_pool = UserPool::new();
 	let mut term = Term::new();
 
 	// lobby
 	loop {
 		// add new connections
 		if let Ok((stream, addr)) = listener.accept() {
-			let id = player_pool.add(Team::Red);
-			net_pool.add(id, stream, addr);
+			user_pool.add(Team::Red, stream, addr);
 			println!("a new player joined");
 		}
 
@@ -34,9 +32,9 @@ fn main() {
 		}
 	}
 
-	let mut w = World::gen(player_pool);
+	let mut w = World::gen(user_pool.get_player_pool().clone());
 
-	net_pool.broadcast(|id| {
+	user_pool.broadcast(|id| {
 		ServerToClientPacket::Init {
 			world: w.clone(),
 			your_id: id,
@@ -45,13 +43,13 @@ fn main() {
 
 	// game
 	loop {
-		for (id, packet) in net_pool.receive_packets().into_iter() {
+		for (id, packet) in user_pool.receive_packets().into_iter() {
 			let command = match packet {
 				ClientToServerPacket::Command(c) => c,
 			};
 
 			if w.checked_exec(id, &command) {
-				net_pool.broadcast(|_| {
+				user_pool.broadcast(|_| {
 					ServerToClientPacket::Command {
 						command: command.clone(),
 						author_id: id,
--- toaserver/src/pool.rs
+++ toaserver/src/pool.rs
@@ -1,33 +1,43 @@
 use std::collections::HashMap;
 use std::net::{SocketAddr, TcpStream};
 
-use toalib::team::PlayerID;
+use toalib::team::{Team, PlayerPool, PlayerID};
 use toalib::packet::{ServerToClientPacket, ClientToServerPacket};
 
 use crate::net::{send_packet, try_receiving_packet};
 
-pub struct NetPool {
-	players: HashMap<PlayerID, (TcpStream, SocketAddr)>,
+struct Entry {
+	stream: TcpStream,
+	addr: SocketAddr,
 }
 
-impl NetPool {
-	pub fn new() -> NetPool {
-		NetPool {
-			players: HashMap::new(),
+pub struct UserPool {
+	users: HashMap<PlayerID, Entry>,
+	player_pool: PlayerPool,
+}
+
+impl UserPool {
+	pub fn new() -> UserPool {
+		UserPool {
+			users: HashMap::new(),
+			player_pool: PlayerPool::new(),
 		}
 	}
 
-	pub fn add(&mut self, player: PlayerID, stream: TcpStream, addr: SocketAddr) {
+	pub fn add(&mut self, team: Team, stream: TcpStream, addr: SocketAddr) {
 		stream.set_nonblocking(true).unwrap();
-		self.players.insert(player, (stream, addr));
+
+		let player_id = self.player_pool.add(team);
+		let entry = Entry { stream, addr };
+		self.users.insert(player_id, entry);
 	}
 
 	pub fn send(&mut self, id: PlayerID, p: ServerToClientPacket) {
-		send_packet(p, &mut self.players.get_mut(&id).unwrap().0);
+		send_packet(p, &mut self.users.get_mut(&id).unwrap().stream);
 	}
 
 	pub fn broadcast<F>(&mut self, f: F) where F: Fn(PlayerID) -> ServerToClientPacket {
-		let v: Vec<PlayerID> = self.players.keys()
+		let v: Vec<PlayerID> = self.users.keys()
 				.cloned()
 				.collect();
 		for &x in v.iter() {
@@ -37,11 +47,15 @@ impl NetPool {
 
 	pub fn receive_packets(&mut self) -> Vec<(PlayerID, ClientToServerPacket)> {
 		let mut v = Vec::new();
-		for (&id, (stream, _)) in self.players.iter_mut() {	
-			if let Some(x) = try_receiving_packet(stream) {
+		for (&id, entry) in self.users.iter_mut() {	
+			if let Some(x) = try_receiving_packet(&mut entry.stream) {
 				v.push((id, x.unwrap()));
 			}
 		}
 		v
+	}
+
+	pub fn get_player_pool(&self) -> &PlayerPool {
+		&self.player_pool
 	}
 }
