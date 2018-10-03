991875f0e01651a418b1d65f2dc7a2c7c48791b9
memoryleak47 <memoryleak47@protonmail.ch>
2018-10-03 12:26:57 +0200

    updated vec.rs, improved render.rs

--- toaclient/src/app/render.rs
+++ toaclient/src/app/render.rs
@@ -1,4 +1,4 @@
-use sfml::graphics::{RenderTarget, RectangleShape, Shape, Color, Transformable, Text, Font};
+use sfml::graphics::{RenderWindow, RenderTarget, RectangleShape, Shape, Color, Transformable, Text, Font};
 
 use toalib::misc::{vector_uf, vector_iu, vector_ui};
 use toalib::config::{MAP_SIZE_X, MAP_SIZE_Y, TILESIZE, TILESIZE_VEC};
@@ -32,14 +32,11 @@ impl App {
 	fn render_terrainmap(&mut self) {
 		for x in 0..MAP_SIZE_X {
 			for y in 0..MAP_SIZE_Y {
-				let posf = Vec2f::new(x as f32, y as f32);
 
+				let pos = Vec2f::new(x as f32, y as f32);
+				let size = Vec2f::with(1.);
 				let texture_id = terrain::get_texture_id(self.world.terrainmap[index2d!(x, y)]);
-				let texture = self.texture_state.get_texture(texture_id);
-				let mut shape = RectangleShape::with_texture(texture);
-				shape.set_position(vec2f_to_sfml((posf - self.focus_position) * TILESIZE + vector_uf(vector2u_to_toa(self.window.size())) / 2.0));
-				shape.set_size(vec2f_to_sfml(TILESIZE_VEC()));
-				self.window.draw(&shape);
+				self.render_texture(pos, size, texture_id);
 			}
 		}
 	}
@@ -47,16 +44,11 @@ impl App {
 	fn render_buildingmap(&mut self) {
 		for x in 0..MAP_SIZE_X {
 			for y in 0..MAP_SIZE_Y {
-				if let Some(ref building) = self.world.buildingmap[index2d!(x, y)]
-							.as_ref() {
-					let posf = Vec2f::new(x as f32, y as f32);
-
+				if let Some(ref building) = self.world.buildingmap[index2d!(x, y)].as_ref() {
+					let pos = Vec2f::new(x as f32, y as f32);
+					let size = Vec2f::new(1., 0.5);
 					let texture_id = building::get_texture_id(building, &self.world.pool);
-					let texture = self.texture_state.get_texture(texture_id);
-					let mut shape = RectangleShape::with_texture(texture);
-					shape.set_position(vec2f_to_sfml((posf - self.focus_position) * TILESIZE + vector_uf(vector2u_to_toa(self.window.size())) / 2.0));
-					shape.set_size(vec2f_to_sfml(Vec2f::new(TILESIZE, TILESIZE/2.0)));
-					self.window.draw(&shape);
+					self.render_texture(pos, size, texture_id);
 				}
 			}
 		}
@@ -69,13 +61,11 @@ impl App {
 						.iter()
 						.next()
 						.is_some() {
-					let posf = Vec2f::new(x as f32, y as f32);
-
-					let texture = self.texture_state.get_texture(TextureId::Bag);
-					let mut shape = RectangleShape::with_texture(texture);
-					shape.set_position(vec2f_to_sfml((posf - self.focus_position) * TILESIZE + vector_uf(vector2u_to_toa(self.window.size())) / 2.0 + Vec2f::new(0.0, 20.0)));
-					shape.set_size(vec2f_to_sfml(Vec2f::new(10., 20.)));
-					self.window.draw(&shape);
+					let raw_pos = Vec2f::new(x as f32, y as f32);
+					let pos = raw_pos + Vec2f::new(0., 0.5);
+					let size = Vec2f::new(0.25, 0.5);
+					let texture_id = TextureId::Bag;
+					self.render_texture(pos, size, texture_id);
 				}
 			}
 		}
@@ -85,13 +75,11 @@ impl App {
 		for x in 0..MAP_SIZE_X {
 			for y in 0..MAP_SIZE_Y {
 				if let Some(ref _unit) = self.world.unitmap[index2d!(x, y)] {
-					let posf = Vec2f::new(x as f32, y as f32);
-
-					let texture_unit = self.texture_state.get_texture(TextureId::Unit);
-					let mut shape = RectangleShape::with_texture(texture_unit);
-					shape.set_position(vec2f_to_sfml((posf - self.focus_position) * TILESIZE + vector_uf(vector2u_to_toa(self.window.size())) / 2.0 + Vec2f::new(10.0, 10.0)));
-					shape.set_size(vec2f_to_sfml(Vec2f::new(20., 30.)));
-					self.window.draw(&shape);
+					let raw_pos = Vec2f::new(x as f32, y as f32);
+					let pos = raw_pos + Vec2f::with(0.25);
+					let size = Vec2f::new(0.5, 0.75);
+					let texture_id = TextureId::Unit;
+					self.render_texture(pos, size, texture_id);
 
 					// TODO draw cloth
 				}
@@ -124,26 +112,19 @@ impl App {
 
 	fn render_marker(&mut self, pos: Vec2u, color: &Color, marker_type: MarkerType) {
 		let halfscreen = Vec2f::new(self.window.size().x as f32, self.window.size().y as f32) / 2.0;
-		let posf = vector_uf(pos) * TILESIZE;
-
-		let left_top = (posf - self.focus_position * TILESIZE) + halfscreen;
 
 		match marker_type {
 			MarkerType::Transparent => {
-				let mut shape = RectangleShape::new();
-
+				let pos = pos.map(|x| x as f32);
+				let size = Vec2f::with(1.);
 				let color = *color - Color::rgba(0, 0, 0, 155);
-
-				shape.set_fill_color(&color);
-				shape.set_position(vec2f_to_sfml(left_top));
-				shape.set_size(vec2f_to_sfml(TILESIZE_VEC()));
-				self.window.draw(&shape);
+				self.render_rectangle(pos, size, color);
 			},
 			MarkerType::Border => {
-				let mut shape = RectangleShape::with_texture(self.texture_state.get_texture(TextureId::Cursor));
-				shape.set_position(vec2f_to_sfml(left_top));
-				shape.set_size(vec2f_to_sfml(TILESIZE_VEC()));
-				self.window.draw(&shape);
+				let pos = pos.map(|x| x as f32);
+				let size = Vec2f::with(1.);
+				let texture_id = TextureId::Cursor;
+				self.render_texture(pos, size, texture_id);
 			},
 		}
 	}
@@ -171,7 +152,26 @@ impl App {
 		format!("{}\n{}", default, v.join("\n"))
 	}
 
+	fn render_rectangle(&mut self, pos: Vec2f, size: Vec2f, color: Color) {
+		let mut shape = RectangleShape::new();
+		shape.set_fill_color(&color);
 
+		render_shape(self.focus_position, &mut self.window, pos, size, shape);
+	}
 
+	fn render_texture(&mut self, pos: Vec2f, size: Vec2f, texture_id: TextureId) {
+		let shape = RectangleShape::with_texture(self.texture_state.get_texture(texture_id));
+		render_shape(self.focus_position, &mut self.window, pos, size, shape);
+	}
+}
 
+fn render_shape(focus_position: Vec2f, window: &mut RenderWindow, pos: Vec2f, size: Vec2f, mut shape: RectangleShape) {
+	let halfscreen = Vec2f::new(window.size().x as f32, window.size().y as f32) / 2.0;
+	let posf = pos * TILESIZE;
+	let left_top = (posf - focus_position * TILESIZE) + halfscreen;
+
+	shape.set_position(vec2f_to_sfml(left_top));
+	shape.set_size(vec2f_to_sfml(size * TILESIZE_VEC()));
+
+	window.draw(&shape);
 }
--- toalib/src/vec.rs
+++ toalib/src/vec.rs
@@ -1,38 +1,59 @@
 use std::ops::{Add, Sub, Mul, Div};
+use std::fmt::{Display, Debug, Error, Formatter};
+use std::hash::{Hash, Hasher};
 
-#[derive(Debug, PartialEq, Clone, Copy)]
 #[derive(Serialize, Deserialize)]
-pub struct Vec2t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
+pub struct Vec2t<T> {
 	pub x: T,
 	pub y: T,
 }
 
+#[allow(dead_code)]
 pub type Vec2f = Vec2t<f32>;
+#[allow(dead_code)]
 pub type Vec2u = Vec2t<u32>;
+#[allow(dead_code)]
 pub type Vec2i = Vec2t<i32>;
 
-impl<T> Vec2t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
+
+impl<T> Vec2t<T> {
 	pub fn new(x: T, y: T) -> Vec2t<T> {
 		Vec2t { x, y }
 	}
 }
 
-impl<T> Add<Vec2t<T>> for Vec2t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
+impl<T: Copy> Vec2t<T> {
+	pub fn with(a: T) -> Vec2t<T> {
+		Vec2t {
+			x: a,
+			y: a,
+		}
+	}
+}
+
+impl<T> Add<Vec2t<T>> for Vec2t<T> where T: Add<Output=T> {
 	type Output = Vec2t<T>;
 
-	fn add(self, rhs: Vec2t<T>) -> Vec2t<T> {
+	fn add(self, other: Vec2t<T>) -> Vec2t<T> {
 		Vec2t::new (
-			self.x + rhs.x,
-			self.y + rhs.y
+			self.x + other.x,
+			self.y + other.y,
 		)
 	}
 }
 
-impl<T> Sub<Vec2t<T>> for Vec2t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
+impl<T> Add<T> for Vec2t<T> where T: Add<Output=T> + Copy {
+	type Output = Vec2t<T>;
+
+	fn add(self, other: T) -> Vec2t<T> {
+		Vec2t::new (
+			self.x + other,
+			self.y + other,
+		)
+	}
+}
+
+impl<T> Sub<Vec2t<T>> for Vec2t<T> where T: Sub<Output=T> {
 	type Output = Vec2t<T>;
 
 	fn sub(self, other: Vec2t<T>) -> Vec2t<T> {
@@ -43,8 +64,18 @@ impl<T> Sub<Vec2t<T>> for Vec2t<T>
 	}
 }
 
-impl<T> Mul<Vec2t<T>> for Vec2t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
+impl<T> Sub<T> for Vec2t<T> where T: Sub<Output=T> + Copy {
+	type Output = Vec2t<T>;
+
+	fn sub(self, other: T) -> Vec2t<T> {
+		Vec2t::new (
+			self.x - other,
+			self.y - other,
+		)
+	}
+}
+
+impl<T> Mul<Vec2t<T>> for Vec2t<T> where T: Mul<Output=T> {
 	type Output = Vec2t<T>;
 
 	fn mul(self, other: Vec2t<T>) -> Vec2t<T> {
@@ -55,8 +86,7 @@ impl<T> Mul<Vec2t<T>> for Vec2t<T>
 	}
 }
 
-impl<T> Mul<T> for Vec2t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
+impl<T> Mul<T> for Vec2t<T> where T: Mul<Output=T> + Copy {
 	type Output = Vec2t<T>;
 
 	fn mul(self, other: T) -> Vec2t<T> {
@@ -67,8 +97,7 @@ impl<T> Mul<T> for Vec2t<T>
 	}
 }
 
-impl<T> Div<Vec2t<T>> for Vec2t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
+impl<T> Div<Vec2t<T>> for Vec2t<T> where T: Div<Output=T> {
 	type Output = Vec2t<T>;
 
 	fn div(self, other: Vec2t<T>) -> Vec2t<T> {
@@ -79,8 +108,7 @@ impl<T> Div<Vec2t<T>> for Vec2t<T>
 	}
 }
 
-impl<T> Div<T> for Vec2t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
+impl<T> Div<T> for Vec2t<T> where T: Div<Output=T> + Copy {
 	type Output = Vec2t<T>;
 
 	fn div(self, other: T) -> Vec2t<T> {
@@ -88,5 +116,61 @@ impl<T> Div<T> for Vec2t<T>
 			self.x / other,
 			self.y / other,
 		)
+	}
+}
+
+impl<T> Hash for Vec2t<T> where T: Hash {
+	fn hash<H: Hasher>(&self, h: &mut H) {
+		self.x.hash(h);
+		self.y.hash(h);
+		h.finish();
+	}
+}
+
+impl<T: PartialEq> PartialEq for Vec2t<T> {
+	fn eq(&self, rhs: &Self) -> bool {
+		(self.x == rhs.x) && (self.y == rhs.y)
+	}
+}
+
+impl<T: Eq> Eq for Vec2t<T> {}
+
+impl<T> Vec2t<T> {
+	pub fn map<U, F: Fn(T) -> U>(self, f: F) -> Vec2t<U> {
+		Vec2t::new(
+			f(self.x),
+			f(self.y),
+		)
+	}
+}
+
+impl<T: Clone> Clone for Vec2t<T> {
+	fn clone(&self) -> Self {
+		Vec2t::new(
+			self.x.clone(),
+			self.y.clone(),
+		)
+	}
+}
+
+impl<T: Copy> Copy for Vec2t<T> { }
+
+impl<T: Display> Display for Vec2t<T> {
+	fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+		let s = format!("Vec2t({}, {})", self.x, self.y);
+		fmt.write_str(&*s)
+	}
+}
+
+impl<T: Debug> Debug for Vec2t<T> {
+	fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+		let s = format!("Vec2t({:?}, {:?})", self.x, self.y);
+		fmt.write_str(&*s)
+	}
+}
+
+impl Vec2f {
+	pub fn magnitude(self) -> f32 {
+		(self.x + self.y).sqrt()
 	}
 }
