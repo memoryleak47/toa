970d1aa717e8d546dc0a603febd1ea77249741fe
memoryleak47 <memoryleak47@protonmail.ch>
2018-10-03 11:39:29 +0200

    made the graphics loading of the client nicer

--- toaclient/src/graphics/mod.rs
+++ toaclient/src/graphics/mod.rs
@@ -1,96 +1,65 @@
-use std::ptr;
-use std::mem;
-
 pub mod terrain;
 pub mod building;
 
 use sfml::graphics::Texture;
 
-#[derive(Copy, Clone)]
-#[repr(usize)]
-pub enum TextureId {
-	GrassTerrain,
-	ForestTerrain,
-	StoneTerrain,
-	IronTerrain,
+macro_rules! setup {
+	($($x:ident : $y:expr),*) => {
 
-	Unit,
-	UnitCloth,
+		#[derive(Copy, Clone)]
+		#[repr(usize)]
+		pub enum TextureId {
+			$($x),*
+		}
 
-	ConstructionBuilding,
-	FarmBuilding,
-	CampBuilding,
-	SawmillBuilding,
-	StoneMineBuilding,
-	IronMineBuilding,
-	WorkshopBuilding,
-	CastleBuilding,
+		pub struct TextureState {
+			wrappers: Vec<Texture>,
+		}
 
-	SpawnerRedBuilding,
-	SpawnerBlueBuilding,
+		fn get_image_path(s: &str) -> String {
+			use toalib::misc::res_dir;
 
-	Bag,
-	Cursor,
-}
+			let mut dir = res_dir();
+			dir.push("image");
+			let path_string = dir.to_str().unwrap();
+			format!("{}/{}", path_string, s)
+		}
 
-const TEXTURE_COUNT: usize = 18;
-
-pub struct TextureState {
-	wrappers: [Texture; TEXTURE_COUNT],
-}
-
-fn get_image_path(s: &str) -> String {
-	use toalib::misc::res_dir;
-
-	let mut dir = res_dir();
-	dir.push("image");
-	let path_string = dir.to_str().unwrap();
-	format!("{}/{}", path_string, s)
-}
-
-impl TextureState {
-	pub fn new() -> TextureState {
-
-		unsafe {
-			let mut wrappers: [Texture; TEXTURE_COUNT] = mem::uninitialized();
-
-			let nope_texture = Texture::from_file(&get_image_path("nope.png")).unwrap();
-
-			macro_rules! load {
-				($a: expr, $b: expr) => {{
-					let texture = Texture::from_file(&get_image_path($b)).unwrap_or(nope_texture.clone());
-					ptr::write(&mut wrappers[$a as usize], texture);
-				}}
+		impl TextureState {
+			pub fn new() -> TextureState {
+				let nope_texture = Texture::from_file(&get_image_path("nope.png")).unwrap();
+				let wrappers = vec![$( Texture::from_file(&get_image_path($y)).unwrap_or(nope_texture.clone())),*];
+				TextureState { wrappers }
 			}
 
-			load!(TextureId::GrassTerrain, "terrain/grass.png");
-			load!(TextureId::ForestTerrain, "terrain/forest.png");
-			load!(TextureId::StoneTerrain, "terrain/stone.png");
-			load!(TextureId::IronTerrain, "terrain/iron.png");
-
-			load!(TextureId::Unit, "unit.png");
-			load!(TextureId::UnitCloth, "unit_cloth.png");
-
-			load!(TextureId::ConstructionBuilding, "building/construction.png");
-			load!(TextureId::FarmBuilding, "building/farm.png");
-			load!(TextureId::CampBuilding, "building/camp.png");
-			load!(TextureId::SawmillBuilding, "building/sawmill.png");
-			load!(TextureId::StoneMineBuilding, "building/stonemine.png");
-			load!(TextureId::IronMineBuilding, "building/ironmine.png");
-			load!(TextureId::WorkshopBuilding, "building/workshop.png");
-			load!(TextureId::CastleBuilding, "building/castle.png");
-
-			load!(TextureId::SpawnerRedBuilding, "building/spawner/red.png");
-			load!(TextureId::SpawnerBlueBuilding, "building/spawner/blue.png");
-
-			load!(TextureId::Bag, "bag.png");
-			load!(TextureId::Cursor, "cursor.png");
-
-			TextureState { wrappers }
+			pub fn get_texture(&self, id: TextureId) -> &Texture {
+				&self.wrappers[id as usize]
+			}
 		}
-	}
-
-	pub fn get_texture(&self, id: TextureId) -> &Texture {
-		&self.wrappers[id as usize]
-	}
+	};
 }
+
+setup!(
+	GrassTerrain: "terrain/grass.png",
+	ForestTerrain: "terrain/forest.png",
+	StoneTerrain: "terrain/stone.png",
+	IronTerrain: "terrain/iron.png",
+
+	Unit: "unit.png",
+	UnitCloth: "unit_cloth.png",
+
+	ConstructionBuilding: "building/construction.png",
+	FarmBuilding: "building/farm.png",
+	CampBuilding: "building/camp.png",
+	SawmillBuilding: "building/sawmill.png",
+	StoneMineBuilding: "building/stonemine.png",
+	IronMineBuilding: "building/ironmine.png",
+	WorkshopBuilding: "building/workshop.png",
+	CastleBuilding: "building/castle.png",
+
+	SpawnerRedBuilding: "building/spawner/red.png",
+	SpawnerBlueBuilding: "building/spawner/blue.png",
+
+	Bag: "bag.png",
+	Cursor: "cursor.png"
+);
