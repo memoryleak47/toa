56a190779bcc7fd971d4fcb54a7a46aec0c063c6
memoryleak47 <memoryleak47@protonmail.ch>
2019-06-25 13:11:53 +0200

    fixed hue

--- toaclient/src/graphics/hue.rs
+++ toaclient/src/graphics/hue.rs
@@ -6,12 +6,14 @@ pub fn hue(tex: &Texture, hue: (u8, u8, u8)) -> Texture {
 	for x in 0..w {
 		for y in 0..h {
 			let mut c = img.pixel_at(x, y);
-			let blue = c.b;
-			c.b = 0;
-			c.r = c.r.saturating_add(hue.0.saturating_mul(blue));
-			c.g = c.g.saturating_add(hue.1.saturating_mul(blue));
-			c.b = c.b.saturating_add(hue.2.saturating_mul(blue));
-			img.set_pixel(x, y, &c);
+			if c.b > c.r.saturating_add(c.g) {
+				let blue = c.b;
+				c.b = 0;
+				c.r = c.r.saturating_add(hue.0.saturating_mul(blue));
+				c.g = c.g.saturating_add(hue.1.saturating_mul(blue));
+				c.b = c.b.saturating_add(hue.2.saturating_mul(blue));
+				img.set_pixel(x, y, &c);
+			}
 		}
 	}
 	Texture::from_image(&img).unwrap()
