3f31b05c73997099bf3503ad2efd88269a2c6236
memoryleak47 <memoryleak47@protonmail.ch>
2019-10-28 02:11:01 +0100

    now tilemap uses an array under the hood

--- toalib/src/tilemap.rs
+++ toalib/src/tilemap.rs
@@ -1,33 +1,24 @@
-use std::iter;
+use serde::{Serialize, Deserialize};
+use serde::ser::Serializer;
+use serde::de::{Deserializer};
 
 use crate::config::{MAP_SIZE_X, MAP_SIZE_Y};
 use crate::vec::Pos;
 
 const C: usize = (MAP_SIZE_X as usize) * (MAP_SIZE_Y as usize);
 
-#[derive(Clone, Serialize, Deserialize)]
-pub struct TileMap<T: Clone>(Vec<T>);
+#[derive(Clone)]
+pub struct TileMap<T: Clone>([T; C]);
 
 impl<T: Clone> TileMap<T> {
 	pub fn new(t: T) -> TileMap<T> {
-		// TODO: make this an array
-		/*
-		use std::mem::{MaybeUninit, transmute};
+		use std::{mem, ptr};
 
-		let tilemap: [MaybeUninit<T>; C] = unsafe {
-			MaybeUninit::uninit().assume_init()
-		};
+        let mut tilemap: [T; C] = unsafe { mem::uninitialized() };
 
-		for e in &mut tilemap[..] {
-			*e = MaybeUninit::new(t.clone());
-		};
-
-		let tilemap: [T; C] = unsafe { transmute::<_, [T; C]>(tilemap) };
-		*/
-
-		let tilemap = iter::repeat(t)
-			.take(C)
-			.collect();
+        for tile in &mut tilemap[..] {
+            unsafe { ptr::write(tile, t.clone()); }
+		}
 
 		TileMap(tilemap)
 	}
@@ -50,6 +41,34 @@ impl<T: Clone> TileMap<T> {
 
 fn pos_to_index(p: Pos) -> usize {
 	((*p).x as usize) + ((*p).y as usize) * (MAP_SIZE_X as usize)
+}
+
+// TODO: fix hacky serialization
+impl<T: Clone + Serialize> Serialize for TileMap<T> {
+	fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+		let v: Vec<T> = (&self.0[..]).iter()
+			.cloned()
+			.collect();
+
+		v.serialize(serializer)
+	}
+}
+
+impl<'de, T: Clone + Deserialize<'de>> Deserialize<'de> for TileMap<T> {
+	fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+		<Vec<T>>::deserialize(deserializer)
+			.map(|v| {
+				use std::{mem, ptr};
+
+				let mut tilemap: [T; C] = unsafe { mem::uninitialized() };
+
+				for (i, tile) in tilemap.iter_mut().enumerate() {
+					unsafe { ptr::write(tile, v[i].clone()); }
+				}
+
+				TileMap(tilemap)
+			})
+    }
 }
 
 #[derive(Clone, Serialize, Deserialize)]
