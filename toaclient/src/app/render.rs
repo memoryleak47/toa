7a845fce83ae3b17ada14a7451a509b7475e7e17
memoryleak47 <memoryleak47@protonmail.ch>
2020-03-19 03:26:25 +0100

    made app/render nicer

--- toaclient/src/app/render.rs
+++ toaclient/src/app/render.rs
@@ -1,16 +1,45 @@
 use sfml::graphics::{RenderTarget, Sprite, Transformable, Text};
 
-use toalib::vec::{Pos, Vec2f, Vec2i};
-use toalib::world::Building;
+use toalib::vec::{Pos, Vec2f};
 
-use crate::graphics::GameObject;
-use crate::graphics::{bag::Bag, unit::{Unit, Cloth}};
+use crate::graphics::{HasTexture, GameObject};
+use crate::graphics::{bag::Bag, unit::Cloth};
 use crate::vec_compat::*;
 use crate::unit_mode::UnitMode;
 use crate::app::App;
 use crate::app::marker::Marker;
 
-// TODO fix useless clones
+
+// sadly, I can't use a normal method, due to borrowing issues
+macro_rules! draw {
+	($self:expr, $p:expr, $go:expr) => {
+		{
+			let pos = $p.to_f() + $go.get_relative_pos();
+			let size = $go.get_size();
+
+			let tilesize = $self.tilesize;
+
+			let texture = $self.texture_state.get_texture($go.get_texture_id());
+			let texsize = texture.size();
+
+			let mut sprite = Sprite::with_texture(texture);
+			if let Some(color) = $go.get_hue() {
+				sprite.set_color(&color);
+			}
+
+			let halfscreen = Vec2f::new($self.window.size().x as f32, $self.window.size().y as f32) / 2.0;
+			let posf = pos * tilesize;
+			let left_top = (posf - $self.focus_position * tilesize) + halfscreen;
+
+			sprite.set_position(vec2f_to_sfml(left_top));
+			let xscale = (size.x * tilesize) / (texsize.x as f32);
+			let yscale = (size.y * tilesize) / (texsize.y as f32);
+			sprite.set_scale(vec2f_to_sfml(Vec2f::new(xscale, yscale)));
+
+			$self.window.draw(&sprite);
+		}
+	};
+}
 
 impl App {
 	pub fn render(&mut self) {
@@ -24,15 +53,14 @@ impl App {
 
 	fn render_terrainmap(&mut self) {
 		for p in Pos::iter_all() {
-			self.render_gameobject(p, &self.world.terrainmap.get(p).clone()); // TODO
+			draw!(self, p, self.world.terrainmap.get(p));
 		}
 	}
 
 	fn render_buildingmap(&mut self) {
 		for p in Pos::iter_all() {
-			if let Some(ref building) = self.world.buildingmap.get(p) {
-				let building: Building = (*building).clone(); // TODO there is a better way!
-				self.render_gameobject(p, &building);
+			if let Some(building) = self.world.buildingmap.get(p) {
+				draw!(self, p, building);
 			}
 		}
 	}
@@ -43,7 +71,7 @@ impl App {
 					.iter()
 					.next()
 					.is_some() {
-				self.render_gameobject(p, &Bag);
+				draw!(self, p, Bag);
 			}
 		}
 	}
@@ -51,34 +79,21 @@ impl App {
 	fn render_unitmap(&mut self) {
 		for p in Pos::iter_all() {
 			if let Some(u) = self.world.unitmap.get(p) {
-				let u: Unit = u.clone(); // TODO
-				self.render_gameobject(p, &u);
-				self.render_gameobject(p, &Cloth(u.owner));
-
-				if let Some(ref main_item) = self.world.unitmap.get(p).unwrap().main_item {
-					let main_item = main_item.clone(); // TODO
-					self.render_gameobject(p, &main_item);
+				draw!(self, p, u);
+				draw!(self, p, &Cloth(u.owner));
+				if let Some(ref main_item) = u.main_item {
+					draw!(self, p, main_item);
 				}
 			}
 		}
 	}
 
 	fn render_markers(&mut self) {
-		self.render_gameobject(self.cursor, &Marker::Normal);
-
-		let opt_tiles: Option<Vec<Vec2i>> = self.unit_mode.as_ref().and_then(|m| {
-			match m {
-				UnitMode::Attack { ref aim } => Some(aim.get_relative_tiles()),
-				_ => None,
-			}
-		});
-
-		let cursor: Pos = self.cursor;
-
-		if let Some(tiles) = opt_tiles {
-			for rel in tiles.into_iter() {
-				if let Some(p) = cursor.map(|x| x + rel) {
-					self.render_gameobject(p, &Marker::Combat);
+		draw!(self, self.cursor, &Marker::Normal);
+		if let Some(UnitMode::Attack { ref aim }) = self.unit_mode {
+			for rel in aim.get_relative_tiles() {
+				if let Some(p) = self.cursor.map(|x| x + rel) {
+					draw!(self, p, Marker::Combat);
 				}
 			}
 		}
@@ -104,31 +119,5 @@ impl App {
 				.map(|x| x.get_text(&self.world))
 				.collect();
 		format!("{}\n{}", default, v.join("\n"))
-	}
-
-	fn render_gameobject<GO: GameObject>(&mut self, p: Pos, go: &GO) {
-		let pos = p.to_f() + go.get_relative_pos();
-		let size = go.get_size();
-
-		let tilesize = self.tilesize;
-
-		let texture = self.texture_state.get_texture(go.get_texture_id());
-		let texsize = texture.size();
-
-		let mut sprite = Sprite::with_texture(texture);
-		if let Some(color) = go.get_hue() {
-			sprite.set_color(&color);
-		}
-
-		let halfscreen = Vec2f::new(self.window.size().x as f32, self.window.size().y as f32) / 2.0;
-		let posf = pos * tilesize;
-		let left_top = (posf - self.focus_position * tilesize) + halfscreen;
-
-		sprite.set_position(vec2f_to_sfml(left_top));
-		let xscale = (size.x * tilesize) / (texsize.x as f32);
-		let yscale = (size.y * tilesize) / (texsize.y as f32);
-		sprite.set_scale(vec2f_to_sfml(Vec2f::new(xscale, yscale)));
-
-		self.window.draw(&sprite);
 	}
 }
