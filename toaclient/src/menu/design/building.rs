812d017e5378fcad56c1f031b0273d2bb5ae734a
memoryleak47 <memoryleak47@protonmail.ch>
2020-09-26 16:15:43 +0200

    started building design

--- toaclient/src/menu/design/building.rs
+++ toaclient/src/menu/design/building.rs
@@ -14,7 +14,27 @@ use crate::menu::{Widget, MenuState, MenuCommand};
 
 impl App {
 	pub(super) fn build_building_pane(&self, offset: Vec2f) -> Vec<Widget> {
+		let cursor = self.cursor;
+
+		let b = if let Some(b) = self.world.buildingmap.get(cursor) { b }
+		else { return Vec::new(); };
+
 		let mut widgets = Vec::new();
+		let ws = self.window_size();
+
+		widgets.push(Widget {
+			pos: ws * (offset + (0.01, 0.05)),
+			size: ws * (0.025, 0.025),
+			draw_type: b.get_info_string().into(),
+			on_click: vec![],
+		});
+
+		widgets.push(Widget {
+			pos: ws * (offset + (0.01, 0.08)),
+			size: ws * 0.025,
+			draw_type: Color::rgb(100, 30, 30).into(),
+			on_click: vec![MenuCommand::Command(Command::UnitCommand{ command: UnitCommand::BurnBuilding, pos: cursor })],
+		});
 
 		widgets
 	}
