47172bf6570aa523f862b073d146238c04758c19
memoryleak47 <memoryleak47@protonmail.ch>
2020-09-26 16:57:03 +0200

    finished TakeItem

--- toaclient/src/menu/design/item.rs
+++ toaclient/src/menu/design/item.rs
@@ -24,6 +24,18 @@ impl App {
 			on_click: vec![MenuCommand::StateChange(MenuState::TakeItem) ],
 		});
 
+		let on_click = |i| {
+			if matches!(self.menu_state, MenuState::TakeItem) {
+				let cmd = UnitCommand::TakeItem(i);
+				vec![
+					MenuCommand::Command(Command::UnitCommand { command: cmd, pos: self.cursor }),
+					MenuCommand::StateChange(MenuState::Normal),
+				]
+			} else {
+				vec![]
+			}
+		};
+
 		let inv = self.world.itemmap.get(self.cursor);
 		for (i, item) in inv.iter().enumerate() {
 			widgets.push(
@@ -31,7 +43,7 @@ impl App {
 					pos: ws * (offset + (0.03 * i as f32 + 0.01, 0.04)),
 					size: ws * (0.025, 0.025),
 					draw_type: item.get_texture_id().into(),
-					on_click: vec![],
+					on_click: on_click(i),
 				},
 			);
 		}
