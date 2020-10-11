238da9ec867bdf0e4ad35a174e841cd0d440bfbc
memoryleak47 <memoryleak47@protonmail.ch>
2020-10-11 14:04:17 +0200

    added "stamina cost summand" entry

--- src/menu/design/terrain.rs
+++ src/menu/design/terrain.rs
@@ -15,11 +15,19 @@ impl App {
 			hotkey: None,
 		});
 
+		widgets.push(Widget {
+			pos: ws * (offset + (0.01, 0.03)),
+			size: ws * (0.025, 0.025),
+			draw_type: format!("stamina cost summand: {}", t.get_stamina_cost()).into(),
+			on_click: vec![],
+			hotkey: None,
+		});
+
 		let cmd = Command::UnitCommand{ command: UnitCommand::TerrainWork, pos: cursor };
 
 		// TODO check whether its a valid command
 		widgets.push(Widget {
-			pos: ws * (offset + (0.01, 0.04)),
+			pos: ws * (offset + (0.01, 0.06)),
 			size: ws * 0.025,
 			draw_type: Color::rgb(10, 10, 10).into(),
 			on_click: vec![MenuCommand::Command(cmd)],
